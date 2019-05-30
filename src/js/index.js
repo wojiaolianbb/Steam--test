25242f79bf7766f99a98c2eae13bde7a9a1d3af4
bb <wojiaolianbb@163.com>
2019-05-30 19:50:04 +0800

    index ending v0.04

--- src/js/index.js
+++ src/js/index.js
@@ -10,18 +10,4 @@ $(function () {
         $('.slider>div').css('left', '-' + '910' * index + 'px');
         $(this).addClass('active').siblings().removeClass('active');
     });
-    var jsonData = [
-        {"img":"../img/轮播1.jpg"},
-        {"img":"../img/轮播2.jpg"},
-        {"img":"../img/轮播3.jpg"},
-        {"img":"../img/轮播4.jpg"},
-        {"img":"../img/轮播5.jpg"},
-        {"img":"../img/轮播6.jpg"},
-        {"img":"../img/轮播7.jpg"},
-        {"img":"../img/轮播8.jpg"},
-        {"img":"../img/轮播9.jpg"},
-        {"img":"../img/轮播10.jpg"}
-    ]
-var bannerAuto = new autoPlay("banner",599,338,jsonData,"fade");
-
 })
