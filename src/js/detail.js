404ce9183b398b792446e8eaa347c01d017e5a82
bb <wojiaolianbb@163.com>
2019-06-01 19:39:38 +0800

    功能完善

---
+++ src/js/detail.js
@@ -0,0 +1,17 @@
+var jsonData = [{
+        "img": "./img/detail.jpg"
+    },
+    {
+        "img": "./img/detail2.jpg"
+    },
+    {
+        "img": "./img/detail3.jpg"
+    },
+    {
+        "img": "./img/detail4.jpg"
+    },
+    {
+        "img": "./img/detail5.jpg"
+    },
+]
+var bannerAuto = new autoPlay("banner", 600, 337, jsonData, "slide");
